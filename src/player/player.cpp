dc37c5243f5bd20c94ce6bbd14f6628ef1a51a43
Zet0x0 <58139125+Zet0x0@users.noreply.github.com>
2025-08-10 21:18:03 +0500

    player.cpp: change how status_display_type of discord activity is if'd else'd

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -336,10 +336,10 @@ void Player::updateDiscordActivity()
         { "start", m_startedListeningAt }
     };
     activity["status_display_type"]
-        = (details.isEmpty()) ? Discord::NAME
-                              : ((QDateTime::currentSecsSinceEpoch() % 180 < 60)
+        = (QDateTime::currentSecsSinceEpoch() % 180 < 60 && !state.isEmpty())
                                      ? Discord::STATE
-                                     : Discord::DETAILS);
+               : (details.isEmpty()) ? Discord::NAME
+                              : Discord::DETAILS;
 
     if (details.isEmpty())
     {
